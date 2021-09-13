ebb82706ec3f47d15ac1aaffcab2e6762e4402c2
luehkenecology <luehken65@gmail.com>
2021-09-13 14:58:09 +0200

    simplify code

--- 00001-data_preperation.R
+++ 00001-data_preperation.R
@@ -47,10 +47,8 @@ all <- bind_rows(bfa_USA1,
 # read data file to standardize mosquito names
 reval_species <- read_excel("data/speciess.xlsx")
 
-#
-A <- data.frame(oldvalue = reval_species$oldvalue)
-
-asti <- data.frame(old = A, 
+# 
+asti <- data.frame(old = reval_species$oldvalue, 
                    new = paste(reval_species$genus_manual, reval_species$species_manual,sep = " "))
 asti <- na.omit(asti)
 all$mosquito_species_new[!is.na(all$mosquito_species)] <- recoderFunc(all$mosquito_species[!is.na(all$mosquito_species)], asti$old, 
